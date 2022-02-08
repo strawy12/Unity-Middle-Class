c193039c7b9716379c9dfbf29d46333044378be1
유하준 <hwaga0714@gmail.com>
2022-02-08 11:19:37 +0900

    레이캐스트 고침

--- Assets/Scripts/PaintShoot.cs
+++ Assets/Scripts/PaintShoot.cs
@@ -9,30 +9,31 @@ public class PaintShoot : MonoBehaviour
     Camera cam;
     void Start()
     {
-        
+
     }
     void Update()
     {
-        Debug.Log("1");
-            Debug.DrawRay(transform.position, Vector2.right, Color.blue,999f); 
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, 15f, LayerMask.GetMask("Block"));
-            if(hit)
-            {
-                this.tilemap.RefreshAllTiles();
+        Debug.DrawRay(transform.position, Vector2.right, Color.blue, 999f);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, 15f, LayerMask.GetMask("Block"));
 
-                int x, y;
-                x = this.tilemap.WorldToCell(hit.transform.position).x;
-                y = this.tilemap.WorldToCell(hit.transform.position).y;
+        if (hit.collider != null)
+        {
+            tilemap.RefreshAllTiles();
 
-                Vector3Int v3Int = new Vector3Int(x, y, 0);
+            int x, y;
+            Vector3 hitPos = hit.point;
+            hitPos.x += hitPos.x >= 0f ? 0.1f : -0.1f;
+            hitPos.y += hitPos.y >= 0f ? 0.1f : -0.1f;
+            x = this.tilemap.WorldToCell(hitPos).x;
+            y = this.tilemap.WorldToCell(hitPos).y;
 
+            Vector3Int v3Int = new Vector3Int(x, y, 0);
 
+            //타일 색 바꿀 때 이게 있어야 하더군요
+            this.tilemap.SetTileFlags(v3Int, TileFlags.None);
 
-                //타일 색 바꿀 때 이게 있어야 하더군요
-                this.tilemap.SetTileFlags(v3Int, TileFlags.None);
-
-                //타일 색 바꾸기
-                this.tilemap.SetColor(v3Int, (Color.red));
-            }
+            //타일 색 바꾸기
+            this.tilemap.SetColor(v3Int, (Color.red));
         }
+    }
 }
