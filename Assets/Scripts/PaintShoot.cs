b473f4b9996bafbb0fa94f0bfd71bd884f069beb
injun2005 <77712212+injun2005@users.noreply.github.com>
2022-02-09 11:14:35 +0900

    대각선 버그 개같이 해결

--- Assets/Scripts/PaintShoot.cs
+++ Assets/Scripts/PaintShoot.cs
@@ -12,6 +12,7 @@ public class PaintShoot : MonoBehaviour
     private Vector2 targetDir;
     private Vector3Int v3Int;
     private RaycastHit2D hit;
+    private Vector3 shootDir;
 
     public int Remaining; //남은 개수
     int tileX, tileY;
@@ -29,7 +30,7 @@ public class PaintShoot : MonoBehaviour
         hit = Physics2D.Raycast(transform.position, targetDir, 999f, LayerMask.GetMask("Block"));
         Debug.DrawRay(transform.position, targetDir * 999, Color.red);
         Vector3 hitPos = hit.point;
-        hitPos += (Vector3)targetDir*0.3f;
+        hitPos += (Vector3)targetDir* 0.01f;
         tileX = this.tilemap.WorldToCell(hitPos).x;
         tileY = this.tilemap.WorldToCell(hitPos).y;
 
@@ -54,46 +55,49 @@ public class PaintShoot : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            ShootDir();
+            Debug.Log("hit:" + hit.point);
+            Debug.Log("v3Int:" + v3Int);
             if (hit.collider != null)
             {
-                tilemap.RefreshAllTiles();
+                if (Remaining > 0)
+                {
+                    Remaining--;
+                    shootDir = new Vector3(hit.point.x - (v3Int.x + 0.5f), hit.point.y - (v3Int.y + 0.5f)).normalized;
+                    if (Mathf.Abs(shootDir.x) == Mathf.Abs(shootDir.y)) { Debug.Log("return"); return; }
+                    ShootDir();
+                    tilemap.RefreshAllTiles();
 
-                //타일 색 바꿀 때 이게 있어야 하더군요
-                this.tilemap.SetTileFlags(v3Int, TileFlags.None);
-                //타일 색 바꾸기
-                this.tilemap.SetColor(v3Int, (Color.red));
+                    //타일 색 바꿀 때 이게 있어야 하더군요
+                    this.tilemap.SetTileFlags(v3Int, TileFlags.None);
+                    //타일 색 바꾸기
+                    this.tilemap.SetColor(v3Int, (Color.red));
+                }
             }
         }
     }
     public void ShootDir()
     {
-        if (Remaining > 0)
+       
+        if (Mathf.Abs(shootDir.x) < Mathf.Abs(shootDir.y))
         {
-            Vector3 dir;
-            dir = new Vector3(hit.point.x - (v3Int.x + 0.5f), hit.point.y - (v3Int.y + 0.5f)).normalized;
-            Remaining--;
-            if (Mathf.Abs(dir.x) < Mathf.Abs(dir.y))
+            if (hit.point.y > v3Int.y + 0.5)
             {
-                if (hit.point.y > v3Int.y + 0.5)
-                {
-                    //Debug.Log("up");
-                }
-                if (hit.point.y < v3Int.y + 0.5)
-                {
-                    //Debug.Log("down");
-                }
+                Debug.Log("up");
             }
-            else
+            if (hit.point.y < v3Int.y + 0.5)
             {
-                if (hit.point.x > v3Int.x + 0.5)
-                {
-                    //Debug.Log("right");
-                }
-                if (hit.point.x < v3Int.x + 0.5)
-                {
-                    //Debug.Log("left");
-                }
+                Debug.Log("down");
+            }
+        }
+        else
+        {
+            if (hit.point.x > v3Int.x + 0.5)
+            {
+                Debug.Log("right");
+            }
+            if (hit.point.x < v3Int.x + 0.5)
+            {
+                Debug.Log("left");
             }
         }
     }
