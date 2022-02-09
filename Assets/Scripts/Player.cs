0a4c2a27d8d9795ee29f746a9fd71327efab3efb
김유진 <yujin050420@gmail.com>
2022-02-09 12:30:51 +0900

    빨간타일 감지 중력바꾸기

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class Player : MonoBehaviour
-{ 
+{
     private Vector2 currentGravityDir;
 
     public float speed;
@@ -14,9 +14,9 @@ public class Player : MonoBehaviour
     Rigidbody2D body; // 컴포넌트에서 RigidBody를 받아올 변수
 
     [SerializeField] private bool isGround = true;
-    [SerializeField] private GameObject player = null;
+    [SerializeField] private GameObject player;
 
-    private Tilemap tilemap;
+    [SerializeField] private Tilemap tilemap;
 
     void Start()
     {
@@ -24,7 +24,6 @@ public class Player : MonoBehaviour
         //GetComponent를 활용하여 body에 해당 오브젝트의 Rigidbody를 넣어준다. 
         currentGravityDir = Vector2.down;
         EventManager.StartListening("CHANGEGRAVITYSTATE", Rotation);
-        tilemap = GetComponent<Tilemap>();
     }
 
     void FixedUpdate()
@@ -32,21 +31,48 @@ public class Player : MonoBehaviour
         Move();
         Jump();
         Gravity();
-        DetectedRayCast();
+        DetectedRaycast();
     }
 
-    RaycastHit hit;
+
     public float maxDistance = 5f;
 
-
-    void DetectedRayCast()
+    void DetectedRaycast()
     {
-        Physics2D.Raycast(transform.position, Vector2.up, maxDistance);
+        RaycastHit2D upHit = Physics2D.Raycast(transform.position, Vector2.up, maxDistance, LayerMask.GetMask("Gravity"));
+        RaycastHit2D downHit = Physics2D.Raycast(transform.position, Vector2.down, maxDistance, LayerMask.GetMask("Gravity"));
+        RaycastHit2D leftHit = Physics2D.Raycast(transform.position, Vector2.left, maxDistance, LayerMask.GetMask("Gravity"));
+        RaycastHit2D rightHit = Physics2D.Raycast(transform.position, Vector2.right, maxDistance, LayerMask.GetMask("Gravity"));
 
         Debug.DrawRay(transform.position, Vector2.up * maxDistance, Color.red);
+        Debug.DrawRay(transform.position, Vector2.down * maxDistance, Color.red);
+        Debug.DrawRay(transform.position, Vector2.left * maxDistance, Color.red);
+        Debug.DrawRay(transform.position, Vector2.right * maxDistance, Color.red);
+
+        GravityPosColor(upHit, GravityState.Up);
+        GravityPosColor(downHit, GravityState.Down);
+        GravityPosColor(leftHit, GravityState.Left);
+        GravityPosColor(rightHit, GravityState.Right);
     }
 
-    void Move() 
+    Vector3Int GravityPosColor(RaycastHit2D detectedRay, GravityState detectType)
+    {
+        int x = tilemap.WorldToCell(detectedRay.point).x;
+        int y = tilemap.WorldToCell(detectedRay.point).y;
+
+        Vector3Int tilepos = new Vector3Int(x, y, 0);
+
+        Debug.Log(tilepos);
+
+        if (tilemap.GetColor(tilepos) == Color.red)
+        {
+            GameManager.Inst.SetGravityState(detectType);
+        }
+
+        return tilepos;
+    }
+
+    void Move()
     {
         float moveX = 0f;
 
@@ -63,7 +89,7 @@ public class Player : MonoBehaviour
         transform.Translate(new Vector2(moveX, 0f) * 0.1f);
     }
 
-    void Jump() 
+    void Jump()
     {
         if (Input.GetKey(KeyCode.Space) && isGround)
         {
@@ -86,7 +112,7 @@ public class Player : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Square"))
-        { 
+        {
             isGround = true;
         }
     }
