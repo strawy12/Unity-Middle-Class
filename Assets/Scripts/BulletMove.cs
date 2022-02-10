72cf4a9d59ae2c091723aa34783c4fcb5b829447
injun2005 <77712212+injun2005@users.noreply.github.com>
2022-02-10 10:35:02 +0900

    bullet bug 수정

--- Assets/Scripts/BulletMove.cs
+++ Assets/Scripts/BulletMove.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BulletMove : MonoBehaviour
 {
     private PaintShoot paintShoot;
-    private float speed = 10f;
+    private float speed = 15f;
     private Vector3 hitPoint;
     private Vector3Int v3Int;
     void Start()
@@ -15,18 +15,30 @@ public class BulletMove : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector2.up * speed * Time.deltaTime);
+        speed += Time.deltaTime * 2f;
+        HItCheck();
     }
-    private void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.CompareTag("Square"))
-        {
-            Destroy(gameObject);
-            paintShoot.ShootDir(hitPoint,v3Int);
-        }
-    }
+    //private void OnCollisionEnter2D(Collision2D col)
+    //{
+    //    if (col.gameObject.CompareTag("Square"))
+    //    {
+    //        Destroy(gameObject);
+    //        Debug.Log("col");
+    //        paintShoot.ShootDir(hitPoint,v3Int);
+    //    }
+    //}
     public void SetVariable(Vector3 hitP, Vector3Int v3I)
     {
         v3Int = v3I;
         hitPoint = hitP;
+    }
+    public void HItCheck()
+    {
+        if(Vector2.Distance(hitPoint,transform.position) < 0.5f)
+        {
+            Destroy(gameObject);
+            Debug.Log("destroy");
+            paintShoot.ShootDir(hitPoint, v3Int);
+        }
     }
 }
